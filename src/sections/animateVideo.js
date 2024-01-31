3730ee4cf28c4b5d879308e227cab3804306a99f
jackytruongpro <jacky-t@hotmail.fr>
2024-01-31 17:18:04 +0100

    Fix animation video

--- src/sections/animateVideo.js
+++ src/sections/animateVideo.js
@@ -4,69 +4,80 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger'
 export function animateVideo(element) {
   element.innerHTML = `
   <div class="animateVideoContainer">
-    <h2>You’ve never seen everything like this before.</h2>
-    <video src="/video/ravioli-can.mp4" playsinline="true" webkit-playsinline="true" preload="auto" muted="muted" class="video-background"></video>
+    <video src="/video/ravioli-can-960.mp4" playsinline="true" webkit-playsinline="true" preload="auto" muted="muted" class="video-background"></video>
     <div id="container"></div>
-  </div>
+  <div>
   `
 
-  const video = document.querySelector('.video-background')
-  let src = video.currentSrc || video.src
-
+  console.clear();
+  /* The encoding is super important here to enable frame-by-frame scrubbing. */
+  
+  // ffmpeg -i ~/Downloads/Toshiba\ video/original.mov -movflags faststart -vcodec libx264 -crf 23 -g 1 -pix_fmt yuv420p output.mp4
+  // ffmpeg -i ~/Downloads/Toshiba\ video/original.mov -vf scale=960:-1 -movflags faststart -vcodec libx264 -crf 20 -g 1 -pix_fmt yuv420p output_960.mp4
+  
+  const video = document.querySelector(".video-background");
+  let src = video.currentSrc || video.src;
+  console.log(video, src);
+  
+  /* Make sure the video is 'activated' on iOS */
   function once(el, event, fn, opts) {
     var onceFn = function (e) {
-      el.removeEventListener(event, onceFn)
-      fn.apply(this, arguments)
-    }
-    el.addEventListener(event, onceFn, opts)
-    return onceFn
+      el.removeEventListener(event, onceFn);
+      fn.apply(this, arguments);
+    };
+    el.addEventListener(event, onceFn, opts);
+    return onceFn;
   }
-
-  once(document.documentElement, 'touchstart', function (e) {
-    video.play()
-    video.pause()
-  })
-
-  gsap.registerPlugin(ScrollTrigger)
-
+  
+  once(document.documentElement, "touchstart", function (e) {
+    video.play();
+    video.pause();
+  });
+  
+  /* ---------------------------------- */
+  /* Scroll Control! */
+  
+  gsap.registerPlugin(ScrollTrigger);
+  
   let tl = gsap.timeline({
     defaults: { duration: 1 },
     scrollTrigger: {
-      trigger: '#container',
-      start: 'top top',
-      end: 'bottom bottom',
+      trigger: "#container",
+      start: "top center",
+      end: "bottom center",
       scrub: true,
-    },
-  })
-
-  once(video, 'loadedmetadata', () => {
+    }
+  });
+  
+  once(video, "loadedmetadata", () => {
     tl.fromTo(
       video,
       {
-        currentTime: 0,
+        currentTime: video.duration || 1
       },
       {
-        currentTime: video.duration || 1,
+        currentTime: 0
       }
-    )
-  })
-
+    );
+  });
+  
+  /* When first coded, the Blobbing was important to ensure the browser wasn't dropping previously played segments, but it doesn't seem to be a problem now. Possibly based on memory availability? */
   setTimeout(function () {
-    if (window['fetch']) {
+    if (window["fetch"]) {
       fetch(src)
         .then((response) => response.blob())
         .then((response) => {
-          var blobURL = URL.createObjectURL(response)
-
-          var t = video.currentTime
-          once(document.documentElement, 'touchstart', function (e) {
-            video.play()
-            video.pause()
-          })
-
-          video.setAttribute('src', blobURL)
-          video.currentTime = t + 0.01
-        })
+          var blobURL = URL.createObjectURL(response);
+  
+          var t = video.currentTime;
+          once(document.documentElement, "touchstart", function (e) {
+            video.play();
+            video.pause();
+          });
+  
+          video.setAttribute("src", blobURL);
+          video.currentTime = t + 0.01;
+        });
     }
-  }, 1000)
+  }, 1000);
 }
