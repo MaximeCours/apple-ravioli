7dcd4ded380c2ebf6e935174bead4d6902919f79
jackytruongpro <74652548+jackytruongpro@users.noreply.github.com>
2024-01-31 15:45:08 +0100

    Add animation can video (#6)
    * Add animation can video
    
    * Fix format
    
    * Remove 200vh on body

--- main.js
+++ main.js
@@ -4,21 +4,24 @@ import './src/css/base.css'
 import './src/css/nav.scss'
 import './src/css/header.scss'
 //import './src/css/heroHeader.scss'
+import './src/css/animateVideo.scss'
 import './src/css/inspiration.scss'
 import './src/css/application.scss'
 import './src/css/story.scss'
 // JS
 import { nav } from './src/sections/nav.js'
-import { heroHeader } from './src/sections/heroHeader.js'
 import { header } from './src/sections/header.js'
+import { heroHeader } from './src/sections/heroHeader.js'
 import { application } from './src/sections/application.js'
 import { story } from './src/sections/story.js'
 import { inspiration } from './src/sections/inspiration.js'
+import { animateVideo } from './src/sections/animateVideo.js'
 
 document.querySelector('#app').innerHTML = `
     <nav></nav>
     <section id="header"></section>
     <section id="heroHeader"></section>
+    <section id="animateVideo"></section>
     <section id="application"></section>
     <section id="story"></section>
     <section id="inspiration"></section>
@@ -27,6 +30,7 @@ document.querySelector('#app').innerHTML = `
 nav(document.querySelector('nav'))
 header(document.querySelector('#header'))
 heroHeader(document.querySelector('#heroHeader'))
+animateVideo(document.querySelector('#animateVideo'))
 application(document.querySelector('#application'))
 story(document.querySelector('#story'))
 inspiration(document.querySelector('#inspiration'))
---
+++ src/sections/animateVideo.js
@@ -0,0 +1,72 @@
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+
+export function animateVideo(element) {
+  element.innerHTML = `
+  <div class="animateVideoContainer">
+    <h2>You’ve never seen everything like this before.</h2>
+    <video src="/video/ravioli-can.mp4" playsinline="true" webkit-playsinline="true" preload="auto" muted="muted" class="video-background"></video>
+    <div id="container"></div>
+  </div>
+  `
+
+  const video = document.querySelector('.video-background')
+  let src = video.currentSrc || video.src
+
+  function once(el, event, fn, opts) {
+    var onceFn = function (e) {
+      el.removeEventListener(event, onceFn)
+      fn.apply(this, arguments)
+    }
+    el.addEventListener(event, onceFn, opts)
+    return onceFn
+  }
+
+  once(document.documentElement, 'touchstart', function (e) {
+    video.play()
+    video.pause()
+  })
+
+  gsap.registerPlugin(ScrollTrigger)
+
+  let tl = gsap.timeline({
+    defaults: { duration: 1 },
+    scrollTrigger: {
+      trigger: '#container',
+      start: 'top top',
+      end: 'bottom bottom',
+      scrub: true,
+    },
+  })
+
+  once(video, 'loadedmetadata', () => {
+    tl.fromTo(
+      video,
+      {
+        currentTime: 0,
+      },
+      {
+        currentTime: video.duration || 1,
+      }
+    )
+  })
+
+  setTimeout(function () {
+    if (window['fetch']) {
+      fetch(src)
+        .then((response) => response.blob())
+        .then((response) => {
+          var blobURL = URL.createObjectURL(response)
+
+          var t = video.currentTime
+          once(document.documentElement, 'touchstart', function (e) {
+            video.play()
+            video.pause()
+          })
+
+          video.setAttribute('src', blobURL)
+          video.currentTime = t + 0.01
+        })
+    }
+  }, 1000)
+}
