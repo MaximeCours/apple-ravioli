6dd4ed7ac918b9f46d93576c3962b858599e4d68
Pierre Antoine Baizeau <pabaizeau@gmail.com>
2024-01-31 12:07:15 +0100

    add header

--- main.js
+++ main.js
@@ -2,15 +2,18 @@
 import './src/css/fonts.css'
 import './src/css/base.css'
 import './src/css/nav.scss'
+import './src/css/header.scss'
 // JS
 import { nav } from './src/sections/nav.js'
 import { heroHeader } from './src/sections/heroHeader.js'
+import { header } from './src/sections/header.js'
 
 document.querySelector('#app').innerHTML = `
     <nav></nav>
-    <h1>Hello world</h1>
+    <section id="header"></section>
     <section id="heroHeader"></section>
 `
 
 nav(document.querySelector('nav'))
+header(document.querySelector('#header'))
 heroHeader(document.querySelector('#heroHeader'))
