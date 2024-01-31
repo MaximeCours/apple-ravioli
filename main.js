179fefe2fa33f8fcf7ce1b04946bec3424c89712
jackytruongpro <jacky-t@hotmail.fr>
2024-01-31 15:02:39 +0100

    Add animation can video

--- main.js
+++ main.js
@@ -2,15 +2,19 @@
 import './src/css/fonts.css'
 import './src/css/base.css'
 import './src/css/nav.scss'
+import './src/css/animateVideo.scss'
 // JS
 import { nav } from './src/sections/nav.js'
 import { heroHeader } from './src/sections/heroHeader.js'
+import { animateVideo } from './src/sections/animateVideo.js';
 
 document.querySelector('#app').innerHTML = `
     <nav></nav>
     <h1>Hello world</h1>
     <section id="heroHeader"></section>
+    <section id="animateVideo"></section>
 `
 
 nav(document.querySelector('nav'))
 heroHeader(document.querySelector('#heroHeader'))
+animateVideo(document.querySelector('#animateVideo'))
