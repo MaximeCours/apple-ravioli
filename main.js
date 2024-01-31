b01533ca1b630e0c57e7e1033e64c947f5f14c3a
Maxime Perraud <contact@maxime-p.dev>
2024-01-31 23:16:50 +0100

    adding footer
    # Conflicts:
    #	main.js
    
    # Conflicts:
    #	main.js

--- main.js
+++ main.js
@@ -8,6 +8,8 @@ import './src/css/animateVideo.scss'
 import './src/css/inspiration.scss'
 import './src/css/application.scss'
 import './src/css/story.scss'
+import './src/css/ravioli.scss'
+import './src/css/footer.scss'
 // JS
 import { nav } from './src/sections/nav.js'
 import { header } from './src/sections/header.js'
@@ -16,6 +18,8 @@ import { application } from './src/sections/application.js'
 import { story } from './src/sections/story.js'
 import { inspiration } from './src/sections/inspiration.js'
 import { animateVideo } from './src/sections/animateVideo.js'
+import { ravioli } from './src/sections/ravioli.js'
+import { footer } from './src/sections/footer.js'
 
 document.querySelector('#app').innerHTML = `
     <nav></nav>
@@ -24,7 +28,9 @@ document.querySelector('#app').innerHTML = `
     <section id="animateVideo"></section>
     <section id="application"></section>
     <section id="story"></section>
+    <section id="ravioli"></section>
     <section id="inspiration"></section>
+    <footer></footer>
 `
 
 nav(document.querySelector('nav'))
@@ -33,4 +39,6 @@ heroHeader(document.querySelector('#heroHeader'))
 animateVideo(document.querySelector('#animateVideo'))
 application(document.querySelector('#application'))
 story(document.querySelector('#story'))
+ravioli(document.querySelector('#ravioli'))
 inspiration(document.querySelector('#inspiration'))
+footer(document.querySelector('footer'))
